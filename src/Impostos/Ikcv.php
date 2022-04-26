fbb5201a75823d661d9ef756ac349c4e0beb2071
imkimura <kimurajulia15@gmail.com>
2022-04-25 21:08:49 -0300

    [TEMPLATE METHOD] FINAL

---
+++ src/Impostos/Ikcv.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kimura\DesignPattern\Impostos;
+
+use Kimura\DesignPattern\Orcamento;
+
+class Ikcv implements ImpostoCom2Aliquotas
+{
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
+    {
+        return $orcamento->valor > 300 && $orcamento->quantidadeItens > 3;
+    }
+
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.04;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.025;
+    }
+}
